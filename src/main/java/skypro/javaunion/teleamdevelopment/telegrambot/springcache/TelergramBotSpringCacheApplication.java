c9ab8e4c95badce359cdd06667d8af14f2970a86
ChernenkiyNikitych4732 <chernenkiy.nikitychsssr@gmail.com>
2024-02-04 11:38:20 +0100

    Курс 4 Домашнее задание 45 Кеширование, работа с кешем (Spring cache) - изначальный комментраий

---
+++ src/main/java/skypro/javaunion/teleamdevelopment/telegrambot/springcache/TelergramBotSpringCacheApplication.java
@@ -0,0 +1,13 @@
+package skypro.javaunion.teleamdevelopment.telegrambot.springcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelergramBotSpringCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelergramBotSpringCacheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/skypro/javaunion/teleamdevelopment/telegrambot/springcache/TelergramBotSpringCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.javaunion.teleamdevelopment.telegrambot.springcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelergramBotSpringCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
